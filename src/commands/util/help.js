34a51a30ceacd86f311cf02a8b0840db669bab83
AcarLAcar <47527803+AcarLAcar@users.noreply.github.com>
2019-03-30 09:09:19 +0300

    Update help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -5,22 +5,21 @@ const { disambiguation } = require('../../util');
 module.exports = class HelpCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'help',
-			group: 'util',
-			memberName: 'help',
-			aliases: ['commands'],
-			description: 'Displays a list of available commands, or detailed information for a specified command.',
+			name: 'yardım',
+			group: 'genel',
+			memberName: 'yardım',
+			aliases: ['commands', 'help', 'y', 'komutlar'],
+			description: 'Komut listesini veya seçilen komut hakkında detaylı bilgileri gösterir.',
 			details: oneLine`
 				The command may be part of a command name or a whole command name.
 				If it isn't specified, all available commands will be listed.
 			`,
-			examples: ['help', 'help prefix'],
+			examples: ['yardım', 'yardım prefix'],
 			guarded: true,
-
 			args: [
 				{
-					key: 'command',
-					prompt: 'Which command would you like to view the help for?',
+					key: 'komut',
+					prompt: 'Hangi komut hakkında bilgi almak istiyorsun?',
 					type: 'string',
 					default: ''
 				}
@@ -28,46 +27,46 @@ module.exports = class HelpCommand extends Command {
 		});
 	}
 
-	async run(msg, args) { // eslint-disable-line complexity
+	async run(msg, args) {
 		const groups = this.client.registry.groups;
-		const commands = this.client.registry.findCommands(args.command, false, msg);
-		const showAll = args.command && args.command.toLowerCase() === 'all';
+		const commands = this.client.registry.findCommands(args.komut, false, msg);
+		const showAll = args.komut && args.command.toLowerCase() === 'hepsi';
 		if(args.command && !showAll) {
 			if(commands.length === 1) {
 				let help = stripIndents`
 					${oneLine`
-						__Command **${commands[0].name}**:__ ${commands[0].description}
-						${commands[0].guildOnly ? ' (Usable only in servers)' : ''}
-						${commands[0].nsfw ? ' (NSFW)' : ''}
+						__**${commands[0].name}** komutu:__ ${commands[0].description}
+						${commands[0].guildOnly ? ' (Sadece sunucularda kullanılabilir)' : ''}
+						${commands[0].nsfw ? ' (NSFW içerikli)' : ''}
 					`}
 
-					**Format:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+					**Kullanım:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
 				`;
-				if(commands[0].aliases.length > 0) help += `\n**Aliases:** ${commands[0].aliases.join(', ')}`;
+				if(commands[0].aliases.length > 0) help += `\n**Alternatifler:** ${commands[0].aliases.join(', ')}`;
 				help += `\n${oneLine`
-					**Group:** ${commands[0].group.name}
+					**Kategori:** ${commands[0].group.name}
 					(\`${commands[0].groupID}:${commands[0].memberName}\`)
 				`}`;
-				if(commands[0].details) help += `\n**Details:** ${commands[0].details}`;
-				if(commands[0].examples) help += `\n**Examples:**\n${commands[0].examples.join('\n')}`;
+				if(commands[0].details) help += `\n**Detaylar:** ${commands[0].details}`;
+				if(commands[0].examples) help += `\n**Örnekler:**\n${commands[0].examples.join('\n')}`;
 
 				const messages = [];
 				try {
 					messages.push(await msg.direct(help));
-					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+					if(msg.channel.type !== 'dm') messages.push(await msg.reply('DM yoluyla mesajları gönderdim.'));
 				} catch(err) {
-					messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+					messages.push(await msg.reply('Sana DM yoluyla mesaj gönderemiyorum. Muhtemelen DM\'lerini kapatmışsın.'));
 				}
 				return messages;
 			} else if(commands.length > 15) {
-				return msg.reply('Multiple commands found. Please be more specific.');
+				return msg.reply('Birden fazla komut bulundu. Lütfen daha seçici ol');
 			} else if(commands.length > 1) {
 				return msg.reply(disambiguation(commands, 'commands'));
 			} else {
 				return msg.reply(
-					`Unable to identify command. Use ${msg.usage(
+					`Bu komutu tanıyamadım. ${msg.usage(
 						null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined
-					)} to view the list of all commands.`
+					)} kullanarak tüm komutları görüntüleyebilirsin.`
 				);
 			}
 		} else {
@@ -75,29 +74,29 @@ module.exports = class HelpCommand extends Command {
 			try {
 				messages.push(await msg.direct(stripIndents`
 					${oneLine`
-						To run a command in ${msg.guild ? msg.guild.name : 'any server'},
-						use ${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						For example, ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
+						Bir komut kullanmak için ${msg.guild ? msg.guild.name : 'özel mesajlarda'}
+						${Command.usage('komut', msg.guild ? msg.guild.commandPrefix : null, this.client.user)} yazmalısın.
+						Örnek olarak, ${Command.usage('yardım', msg.guild ? msg.guild.commandPrefix : null, this.client.user)} yazabilirsin.
 					`}
-					To run a command in this DM, simply use ${Command.usage('command', null, null)} with no prefix.
+					Ayrıca bir komut kullanmak için DM'lerde sadece ${Command.usage('komut', null, null)} kullanabilirsin..
 
-					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
-					Use ${this.usage('all', null, null)} to view a list of *all* commands, not just available ones.
+					${this.usage('<komut>', null, null)} kullanarak seçilen komut hakkında bilgi alabilirsin.
+					${this.usage('hepsi', null, null)} kullanarak *bütün* komutları görüntüleyebilirsin, sadece kullanabilir olanları değil.
 
-					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
+					__**${showAll ? 'Bütün komutlar' : `${msg.guild ? `${msg.guild} adlı sunucuda` : 'DM\'lerde'}`} kullanılabilen komutlar**__
 
-					${(showAll ? groups : groups.filter(grp => grp.commands.some(cmd => cmd.isUsable(msg))))
+					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg))))
 						.map(grp => stripIndents`
 							__${grp.name}__
-							${(showAll ? grp.commands : grp.commands.filter(cmd => cmd.isUsable(msg)))
-								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
+							${grp.commands.filter(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg)))
+								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW içerikli)' : ''}`).join('\n')
 							}
 						`).join('\n\n')
 					}
 				`, { split: true }));
-				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+				if(msg.channel.type !== 'dm') messages.push(await msg.reply('DM yoluyla mesajları gönderdim.'));
 			} catch(err) {
-				messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+				messages.push(await msg.reply('Sana DM yoluyla mesaj gönderemiyorum. Muhtemelen DM\'lerini kapatmışsın.'));
 			}
 			return messages;
 		}
